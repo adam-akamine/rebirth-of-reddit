f2fb3dcf696234b068a4015ec9904e03b0c3854d
kekuso <kekuso808@gmail.com>
2016-06-11 15:07:30 -1000

    Gulp, SASS, and scss functionality added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('watch', function() {
+gulp.task('styles', function() {
+  gulp
+    .src('scss/rebirth.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
+gulp.task('watch', ['styles'], function() {
   gulp.watch('js/*', function() {
     console.log("the file changed");
   });
+  gulp.watch('scss/**/*', ['styles']);
 });
 gulp.task('default', function() {
   console.log("hello");
 });
-
